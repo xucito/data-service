ee9b3d00916368001b6f02f3ebac775a4bd0d631
kozhevnikov-peter <pkozhevnikov@wavesplatform.com>
2019-02-25 20:37:41 +0300

    Test

--- src/endpoints/_common/filters/index.ts
+++ src/endpoints/_common/filters/index.ts
@@ -1,9 +1,13 @@
-import { compose, reject, isNil, mapObjIndexed } from 'ramda';
+import { reject, isNil, mapObjIndexed } from 'ramda';
 
-export const parseFilterValues = parsers => values =>
-  compose(
-    reject(isNil),
-    mapObjIndexed((val, key) => val(values[key]))
-  )(parsers);
+export type Parser<T> = (value: string) => T;
+
+export const parseFilterValues = <Parsers extends Record<string, Parser<any>>>(
+  parsers: Parsers
+) => (
+  values: {
+    [K in keyof Parsers]: Parsers[K] extends Parser<infer R> ? R : never
+  }
+) => reject(isNil, mapObjIndexed((val, key) => val(values[key]), parsers));
 
 export * from './parsers';
