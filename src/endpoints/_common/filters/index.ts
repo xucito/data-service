6134509f0a25b6b677a2bf098062f3dc5ab417e5
kozhevnikov-peter <pkozhevnikov@wavesplatform.com>
2019-02-26 17:15:03 +0300

    Temp

--- src/endpoints/_common/filters/index.ts
+++ src/endpoints/_common/filters/index.ts
@@ -2,35 +2,14 @@ import { reject, isNil, mapObjIndexed } from 'ramda';
 
 export type Parser<T> = (value: string) => T;
 
-// export const parseFilterValues = <Parsers extends Record<string, Parser<any>>>(
-//   parsers: Parsers
-// ) => (
-//   values: {
-//     [K in keyof Parsers]?: Parsers[K] extends Parser<infer R> ? R : never
-//   }
-// ) => reject(isNil, mapObjIndexed((val, key) => val(values[key]), parsers));
-
-type T1 = { q: number, w: string };
-type T2 = keyof T1;
-type T3 = Record<keyof T1, string>;
-type T4 = { [K in keyof T3]: T1[K] };
-
-type T5 = { q: string, e: string };
-type T6 = keyof T1 & keyof T5;
-
-export const parseFilterValues = <Parsers extends Record<string, Parser<any>>>(
+export const parseFilterValues = <
+  Parsers extends Record<string, (s: string) => any>
+>(
   parsers: Parsers
-) => (
-  values: { [K in keyof Parsers]?: string }
-): ({ [K in keyof typeof values]: Parsers[K] extends Parser<infer R> ? R : never }) => null
-  // reject(isNil, mapObjIndexed((val, key) => val(values[key]), parsers));
-
-
-const parsers = {
-  q: (s: string) => 2,
-  w: (s: string) => '2',
-}
-
-const t = parseFilterValues(parsers)({ q: "qwe" });
+) => <ValuesKeys extends keyof Parsers>(
+  values: Partial<Record<ValuesKeys, string>>
+): { [K in ValuesKeys]: ReturnType<Parsers[K]> } => values.map(v => v);
+// mapObjIndexed((val: string, key) => val(values[key]), parsers);
+// reject(isNil, mapObjIndexed((val, key) => val(values[key]), parsers));
 
 export * from './parsers';
